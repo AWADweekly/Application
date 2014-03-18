feea7a68fd057a86e26333ad275349b6c844ce31
jampry <jamie.pryde.09@aberdeen.ac.uk>
2014-03-18 02:38:12 +0000

    Delete twitter_credentials.rb

--- app/views/shared/twitter_credentials.rb
+++
@@ -1,8 +0,0 @@
-require 'twitter'
-
-Twitter.configure do |config|
-  config.consumer_key = ' g9OcdJE7XVhalf6RODzyiA'
-  config.consumer_secret = ' WEVgyoSfn6CCQOFpkyJnJthUTWQjYOYfRoM5Aa7gHQ'
-  config.access_token = '19882793-VmoMahBCnrUZugqrvyd8aGWJRVeAZza9ZajepaTz2'
-  config.access_token_secret = 'l7K3ySnzSfrac5SImE7xxviVpg7Mhrgxgub79VYxaqg0Q'
-end
