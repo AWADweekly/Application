90bebfc4d88d4618cff470ed8212d73659f8b56d
J P <jamie.pryde.09@aberdeen.ac.uk>
2014-03-18 03:07:20 +0000

    removed broken twitter stuff, app works again

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,4 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery
-  before_filter :load_tweets
-
-  def load_tweets
-    @tweets = Twitter.user_timeline[0..4]
-  end
 end
