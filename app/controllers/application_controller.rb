772826348ebbad660d47309588d20e716741616e
J P <jamie.pryde.09@aberdeen.ac.uk>
2014-03-18 02:08:14 +0000

    added twitter, not yet working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  protect_from_forgery
+  before_filter :load_tweets
+
+  def load_tweets
+    @tweets = Twitter.user_timeline[0..4]
+  end
 end
